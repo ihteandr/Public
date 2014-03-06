0d9f27f43207b70b8e6c579e4899ed8d768f201c
ihteandr <ghukasyan.vladimir@gmail.com>
2014-03-06 07:57:24 -0800

    add main js

--- InchUzem/server/public/version_1.0/app/js/main.js
+++ InchUzem/server/public/version_1.0/app/js/main.js
@@ -1,3 +1,25 @@
 /**
  * Created by Vladimir on 10/27/13.
  */
+require.config({
+    baseUrl: 'app',
+    paths: {
+        'jquery': 'lib/jquery/dist/jquery',
+        'angular': 'lib/angular/angular',
+        'text': 'lib/requirejs-text/text'
+    },
+    shim: {
+        'jquery': {
+            exports: 'jQuery'
+        },
+        'angular': {
+            exports: 'angular',
+            'deps': ['jquery']
+        }
+    }
+});
+
+require([
+], function () { 
+    
+})
