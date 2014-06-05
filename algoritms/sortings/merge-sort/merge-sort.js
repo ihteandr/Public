cf464963ab8af1aaaf108753379a3eaa96973670
ihteandr <ghukasyan.vladimir@gmail.com>
2014-06-05 17:20:46 +0400

    rename folders, add merge-sort
    rename bubble-sorting and selection-sorting folder, define merge sorting
    algoritm

---
+++ algoritms/sortings/merge-sort/merge-sort.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Fujitsu on 6/5/2014.
+ */
+function swap(arr, first, second){
+    var temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+}
+
+function merge(left, right){
+    var result = [],
+        li = 0,
+        ri = 0;
+    while(li < left.length && ri < right.length){
+        if(left[li] < right[ri]){
+            result.push(left[li++]);
+        }else{
+            result.push(right[ri++]);
+        }
+    }
+
+    return result.concat(left.slice(li)).concat(right.slice(ri));
+}
+
+function merge_sort(arr){
+    if(arr.length < 2 ){
+        return arr;
+    }
+    var middle = Math.floor(arr.length/2),
+        left = arr.slice(0,middle),
+        right = arr.slice(middle);
+    return merge(merge_sort(left), merge_sort(right));
+}
