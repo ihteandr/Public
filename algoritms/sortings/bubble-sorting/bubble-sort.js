4aaf22b29574bb8718e45fc826af8c0c4cbf5e7a
ihteandr <ghukasyan.vladimir@gmail.com>
2014-06-05 13:19:11 +0400

    update bubble-sort
    define algoritm

--- algoritms/sortings/bubble-sorting/bubble-sort.js
+++ algoritms/sortings/bubble-sorting/bubble-sort.js
@@ -1,6 +1,21 @@
 /**
  * Created by Fujitsu on 6/4/2014.
  */
+
+function swap(arr, firstIndex, secondIndex){
+    var temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+}
+
 function bubble_sorting(arr){
-    
+    var len = arr.length,
+        i, j, stop;
+    for(i = 0; i < len; i++){
+        for(j=0, stop = len-i; j < stop;j++){
+            if(arr[j]<arr[j+1]){
+                swap(arr, j, j+1);
+            }
+        }
+    }
 }
