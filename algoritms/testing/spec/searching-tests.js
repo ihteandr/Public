63a98a70e7448afa66b1c0d4d35716c8664419f6
ihteandr <ghukasyan.vladimir@gmail.com>
2014-06-06 15:53:30 +0400

    add insertion-sort.js, update tests
    define insertion sort, define tests for them

--- algoritms/testing/spec/searching-tests.js
+++ algoritms/testing/spec/searching-tests.js
@@ -2,7 +2,7 @@
  * Created by Fujitsu on 6/5/2014.
  */
 describe("searching tests", function(){
-    it("binary search test", function(){
+    it("binary search", function(){
         expect(binary_search([1,2,3,4,5,6,7,8,9,10,11],11)).toBe(10)
     })
 })
--- algoritms/testing/spec/sorting-tests.js
+++ algoritms/testing/spec/sorting-tests.js
@@ -2,19 +2,23 @@
  * Created by Fujitsu on 6/5/2014.
  */
 describe("Sorting tests", function(){
-    it("bubble sort test", function(){
+    it("bubble sort", function(){
         expect(bubble_sorting([1,5,10,6,7,2,3,4,8,9])).toEqual([1,2,3,4,5,6,7,8,9,10])
     })
 
-    it("merge sort test", function(){
+    it("merge sort", function(){
         expect(merge_sort([1,5,10,6,7,2,3,4,8,9])).toEqual([1,2,3,4,5,6,7,8,9,10])
     })
 
-    it("quick sort test", function(){
+    it("quick sort", function(){
         expect(quick_sort([1,5,10,6,7,2,3,4,8,9])).toEqual([1,2,3,4,5,6,7,8,9,10])
     })
 
-    it("selection sort test", function(){
+    it("selection sort", function(){
         expect(selection_sort([1,5,10,6,7,2,3,4,8,9])).toEqual([1,2,3,4,5,6,7,8,9,10])
+    })
+
+    it("insertion sort", function(){
+        expect(insertion_sort([1,5,10,6,7,2,3,4,8,9])).toEqual([1,2,3,4,5,6,7,8,9,10])
     })
 })
